aca4d1fbe53be3e17bafd093095d525d5dd353ee
Erikka A <114741859+Akkirea@users.noreply.github.com>
2022-11-16 11:37:39 +0800

    Commit initial files

---
+++ index.js
@@ -0,0 +1,33 @@
+let count = 0
+
+let numEl = document.getElementById("num1-el")
+
+
+
+function addOne() {
+  document.getElementById("num1-el").textContent = count += 1
+
+}
+
+function addTwo() {
+  document.getElementById("num1-el").textContent = count += 2
+
+}
+
+function addThree() {
+  document.getElementById("num1-el").textContent = count += 3
+}
+
+// ****GUEST****
+
+function guestOne() {
+  document.getElementById("guest-el").textContent = count += 1
+}
+
+function guestTwo() {
+  document.getElementById("guest-el").textContent = count += 2
+}
+
+function guestThree() {
+  document.getElementById("guest-el").textContent = count += 3
+}
